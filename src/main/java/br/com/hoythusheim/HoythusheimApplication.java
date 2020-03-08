593020c74f84b67cb6a7a369f3e4a50a63c576a1
Cristierre Gomes Konrath <cristierre.konrath@dbserver.com.br>
2020-03-08 13:09:53 -0300

    configuração base spring

---
+++ src/main/java/br/com/hoythusheim/HoythusheimApplication.java
@@ -0,0 +1,13 @@
+package br.com.hoythusheim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoythusheimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoythusheimApplication.class, args);
+	}
+
+}

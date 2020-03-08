d9863a970e0a513beafe25427a01f632a209bdef
Cristierre Gomes Konrath <cristierre.konrath@dbserver.com.br>
2020-03-08 13:47:21 -0300

    Cria classe ContabilidadeModel e insere dependencia lombok

---
+++ src/main/java/br/com/hoythusheim/models/ContabilidadeModel.java
@@ -0,0 +1,37 @@
+package br.com.hoythusheim.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
+@Entity
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ContabilidadeModel {
+
+    public ContabilidadeModel() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @NotEmpty
+    private String nomeConta;
+
+    @NotEmpty
+    private Double valor;
+
+    private Date diaRecebimento;
+    private Date diaVencimento;
+    private Date diaPagamento;
+    private String statusConta;
+
+
+}
